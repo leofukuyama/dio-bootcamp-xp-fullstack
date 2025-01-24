eb0748e710d7ae033245e5b1e3781ea97208210d
leofukuyama <leofukuyama22@gmail.com>
2025-01-24 17:56:48 -0300

    Estudo de Estruturas de Repetição

--- Sintaxe Básica com .NET C#/Program.cs
+++ Sintaxe Básica com .NET C#/Program.cs
@@ -1,5 +1,76 @@
 ﻿using Sintaxe_Básica_com_.NET_C_.Models;
 
+string opcao;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+        case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+        case "3":
+            Console.WriteLine("Apagar cliete");
+            break;
+        case "4":
+            Console.WriteLine("Encerrar");
+            // Environment.Exit(0); Finaliza totalmente o programa
+            exibirMenu = false;
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+Console.WriteLine("O programa se encerrou");
+
+// int soma = 0;
+// int numero = 0;
+
+// do
+// {
+//     Console.WriteLine("Digite um número: (0 para parar)");
+//     numero = Convert.ToInt32(Console.ReadLine());
+
+//     soma += numero;
+// } while (numero != 0);
+
+// Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+
+// int numero = 5;
+// int contador = 0;
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($"{contador}º Execução: {numero} x {contador} = {numero * contador}");
+//     contador++;
+
+//     if (contador == 6)
+//     {
+//         break;
+//     }
+// }
+
+// int numero = 5;
+
+// for (int contador = 0; contador <= 10; contador++)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
+
 // Calculadora calc = new Calculadora();
 
 // calc.Somar(10, 30);
