ea62490001c992d6cae5c43a46d73caba2b320ce
leofukuyama <leofukuyama22@gmail.com>
2025-01-30 15:24:18 -0300

    Estudo de Propriedades e Métodos

---
+++ Explorando/Program.cs
@@ -0,0 +1,23 @@
+﻿using Explorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Leonardo";
+p1.Sobrenome = "Oliveira";
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Eduardo";
+p2.Sobrenome = "Neves Queiroz";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Leonardo";
+// p1.Sobrenome = "Oliveira";
+// p1.Idade = 123;
+// p1.Apresentar();
