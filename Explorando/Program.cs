54c9a99ec6bf60d602086a1e04c04facdc04dc86
leofukuyama <leofukuyama22@gmail.com>
2025-02-03 17:58:40 -0300

    Estudo de DateTime e Exceções

--- Explorando/Program.cs
+++ Explorando/Program.cs
@@ -1,21 +1,77 @@
 ﻿using Explorando.Models;
 using System.Globalization;
 
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+new ExemploExcecao().Metodo1();
 
-decimal valorMonetario = 1582.40M;
-Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
-Console.WriteLine(valorMonetario.ToString("N2"));
-
-double porcentagem = .3421;
-Console.WriteLine(porcentagem.ToString("P"));
-
-int numero = 123456;
-Console.WriteLine(numero.ToString("##-##-##"));
+Console.WriteLine("Deu certo!");
 
 
 
+// try
+// {
+//     string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
 
+//     foreach (string linha in linhas)
+//     {
+//         Console.WriteLine(linha);
+//     }
+// } 
+// catch (FileNotFoundException ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+// }
+// catch (DirectoryNotFoundException ex)
+// {
+//     Console.WriteLine("Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado.");
+// }
+// catch(Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+// }
+// finally
+// {
+//     Console.WriteLine("Eu sempre executo!");
+// }
+
+// Console.WriteLine("Chegou até aqui!");
+
+// string dataString = "2022-04-15 19:00";
+
+// bool sucesso = DateTime.TryParseExact(dataString, 
+//                             "yyyy-MM-dd HH:mm", 
+//                             CultureInfo.InvariantCulture, 
+//                             DateTimeStyles.None, 
+//                             out DateTime data);
+
+// // DateTime data = DateTime.Parse(dataString);
+
+
+// if (sucesso)
+// {
+//     Console.WriteLine($"Conversão com sucesso! Data: {data}");
+// }
+// else
+// {
+//     Console.WriteLine($"{dataString} não é uma data válida!");
+// }
+
+// DateTime data = DateTime.Now; // Converter ---> DateTime data = DateTime.Parse("31/04/2022 18:00");
+// Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
+
+// Console.WriteLine(data.ToShortDateString());
+// Console.WriteLine(data.ToShortTimeString());
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+// decimal valorMonetario = 1582.40M;
+// Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+// Console.WriteLine(valorMonetario.ToString("N2"));
+
+// double porcentagem = .3421;
+// Console.WriteLine(porcentagem.ToString("P"));
+
+// int numero = 123456;
+// Console.WriteLine(numero.ToString("##-##-##"));
 
 // string numero1 = "10";
 // string numero2 = "20";
