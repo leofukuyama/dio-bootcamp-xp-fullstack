747b67f6b54e3a1a6f65d7fd0aca74759f2fad0d
leofukuyama <leofukuyama22@gmail.com>
2025-02-04 16:17:29 -0300

    Estudo de Filas, Pilhas e Dicionários

--- Explorando/Program.cs
+++ Explorando/Program.cs
@@ -1,11 +1,91 @@
 ﻿using Explorando.Models;
 using System.Globalization;
 
-new ExemploExcecao().Metodo1();
+Dictionary<string, string> estados = new Dictionary<string, string>();
 
-Console.WriteLine("Deu certo!");
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
 
+foreach (var item in estados) // Ou KeyValuePair<string, string> item, "var" significa que vai reconhecer o tipo automaticamente
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
 
+// estados.Add("BA2", "Bahia");
+
+Console.WriteLine("--------------");
+
+estados.Remove("BA");
+estados["SP"] = "São Paulo - valor alterado";
+
+foreach(var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+Console.WriteLine("--------------");
+
+string chave = "SP2";
+Console.WriteLine($"Verificando o elemento: {chave}");
+
+if (estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente: {chave}");
+}
+else
+{
+    Console.WriteLine($"Valor não existe. É seguro adicionar a chave: {chave}");
+}
+
+Console.WriteLine("--------------");
+
+Console.WriteLine(estados["MG"]);
+
+// Stack<int> pilha = new Stack<int>();
+
+// pilha.Push(4);
+// pilha.Push(6);
+// pilha.Push(8);
+// pilha.Push(10);
+
+// foreach (int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+
+// pilha.Push(20);
+
+// foreach (int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Queue<int> fila = new Queue<int>();
+
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
+
+// foreach (int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+// fila.Enqueue(10);
+
+// foreach (int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+// new ExemploExcecao().Metodo1();
+
+// Console.WriteLine("Deu certo!");
 
 // try
 // {
