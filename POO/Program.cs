2c2245459ca175ce9b0f21de5f80fde52c38d2f4
leofukuyama <leofukuyama22@gmail.com>
2025-02-07 20:41:39 -0300

    Estudo de Abstração e Encapsulamento

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,7 +1,13 @@
 ﻿using POO.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Leonardo";
-p1.Idade = 22;
+ContaCorrente c1 = new ContaCorrente(123, 1000);
 
-p1.Apresentar();
+c1.ExibirSaldo();
+c1.Sacar(5000);
+c1.ExibirSaldo();
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Leonardo";
+// p1.Idade = 22;
+
+// p1.Apresentar();
