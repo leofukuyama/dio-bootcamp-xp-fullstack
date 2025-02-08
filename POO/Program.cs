16084e70321935c24edc1ec96bd1c43dd2ac8357
leofukuyama <leofukuyama22@gmail.com>
2025-02-07 22:08:52 -0300

    Estudo de Herança e Polimorfismo

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,10 +1,26 @@
 ﻿using POO.Models;
 
-ContaCorrente c1 = new ContaCorrente(123, 1000);
+Aluno a1 = new Aluno();
+a1.Nome = "Leonardo";
+a1.Idade = 22;
 
-c1.ExibirSaldo();
-c1.Sacar(5000);
-c1.ExibirSaldo();
+a1.Email = "leonardo@email.com";
+a1.Nota = 10;
+
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Eduardo";
+p1.Idade = 30;
+p1.Salario = 1000;
+
+p1.Apresentar();
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(5000);
+// c1.ExibirSaldo();
 
 // Pessoa p1 = new Pessoa();
 // p1.Nome = "Leonardo";
