ac7520160bcdc2833ba84aae9fedba636feb0433
leofukuyama <leofukuyama22@gmail.com>
2025-02-21 11:53:39 -0300

    feat: Entity Framework Core Connection and Create

--- ModuloAPI/ExemploInicial/Program.cs
+++ ModuloAPI/ExemploInicial/Program.cs
@@ -1,8 +1,12 @@
-using Modulo_API.Controllers;
+using Modulo_API.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
