5b475007097d40e996f8bfa373e16ae37bbe3fcb
leofukuyama <leofukuyama22@gmail.com>
2025-03-18 16:41:44 -0300

    feat: Estudo de Testes Theory e Fact

---
+++ Testes/ModuloTestes/CalculadoraTestes/CalculadoraTests.cs
@@ -0,0 +1,69 @@
+﻿using Calculadora.Services;
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTests
+{
+    private CalculadoraImp _calc;
+
+    public CalculadoraTests()
+    {
+        _calc = new CalculadoraImp();
+    }
+
+    [Fact]
+    public void DeveSomar5Com10ERetornar15()
+    {
+        // Arrange
+        int num1 = 5;
+        int num2 = 10;
+        
+        // Act
+        int resultado = _calc.Somar(num1, num2);
+
+        // Assert
+        Assert.Equal(15, resultado);
+    }
+
+    [Fact]
+    public void DeveSomar10Com10ERetornar20()
+    {
+        // Arrange
+        int num1 = 10;
+        int num2 = 10;
+        
+        // Act
+        int resultado = _calc.Somar(num1, num2);
+
+        // Assert
+        Assert.Equal(20, resultado);
+    }
+
+    [Fact]
+    public void DeveVerificarSe4EhParERetornarVerdadeiro()
+    {
+        // Arrange
+        int numero = 4;
+
+        // Act
+        bool resultado = _calc.EhPar(numero);
+
+        // Assert
+        Assert.True(resultado);
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(8)]
+    [InlineData(10)]
+    public void DeveVerificarSeOsNumeroSaoParesERetornarVerdadeiro(int numero)
+    {
+        // Act
+        bool resultado = _calc.EhPar(numero);
+
+        // Assert
+        Assert.True(resultado);
+    }
+}
