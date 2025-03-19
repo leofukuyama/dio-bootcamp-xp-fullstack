44a1f6a3ead93faf30de618702c26a0d7fa1e954
leofukuyama <leofukuyama22@gmail.com>
2025-03-19 10:52:29 -0300

    feat: Solução Desafio Tests

--- Testes/DesafioTestes/Console/Services/ValidacoesLista.cs
+++ Testes/DesafioTestes/Console/Services/ValidacoesLista.cs
@@ -1,4 +1,9 @@
-namespace TestesUnitarios.Desafio.Console.Services
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Console.Services
 {
     public class ValidacoesLista
     {
